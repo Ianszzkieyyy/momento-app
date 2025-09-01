ce116155c486fa294e3234c55b10c07e24922578
Ianszzkieyyy <ian.agustin071729@gmail.com>
2025-09-02 01:52:13 +0800

    added initial upload logic, will continue

--- components/empty-view.tsx
+++ components/empty-view.tsx
@@ -1,9 +1,32 @@
 "use client"
 
-import { Camera } from "lucide-react";
+import { useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { Camera, User } from "lucide-react";
+import { set } from "date-fns";
 
 export default function EmptyView() {
-    const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [uploading, setUploading] = useState<boolean>(false);
+    const [image, setImage] = useState<File | null>(null);
+
+    const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const supabase = createClient()
+        const file = event.target.files?.[0]
+        if (!file) return
+
+        setUploading(true)
+
+        const fileExt = file.name.split('.').pop()
+        const fileName = `${Math.random()}-${Date.now()}.${fileExt}`
+        const filePath = `entry-photo/${fileName}`
+
+        const { error: uploadError } = await supabase.storage
+            .from("entry-photo")
+            .upload(filePath, file)
+        if (uploadError) {
+            console.error("Error uploading image: ", uploadError)
+            setUploading(false)
+        }
 
     }
 
