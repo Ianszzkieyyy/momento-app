1010af6a182b1745e38f1766f8e156f6034634ad
Ianszzkieyyy <ian.agustin071729@gmail.com>
2025-09-27 23:15:48 +0800

    added barebones calendar

--- app/(main)/calendar/page.tsx
+++ app/(main)/calendar/page.tsx
@@ -1,7 +1,12 @@
+import { Calendar } from "@/components/ui/calendar"
+
 export default function CalendarPage() {
     return (
         <div>
-            Calendar Page
+            <Calendar 
+                mode="single"
+                className="w-screen px-8"
+            />
         </div>
     )
 }
